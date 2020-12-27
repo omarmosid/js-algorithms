260fe10223b43d3702bc69637a3fed57838fd51a
Mohammad Omar <omar.mo.sid@gmail.com>
2020-12-27 12:37:14 +0530

    update two sum

--- src/leetcode/1-two-sum/two-sum.js
+++ src/leetcode/1-two-sum/two-sum.js
@@ -3,18 +3,21 @@
  * @param {number} target
  * @return {number[]}
  */
-const twoSum = function(nums, target) {
-    const hashMap = {};
-    for(i=0; i < nums.length; i++) {
-        let currentNum = nums[i];
-        let neededValue = target - currentNum;
-        let firstNumberIndex = hashMap[neededValue];
-        if(firstNumberIndex != null) {
-            return [firstNumberIndex, i];
-        } else {
-            hashMap[currentNum] = i;
-        }
+const twoSum = function (nums, target) {
+  const hashMap = {}; // map of neededValues and their indexes, so basically something like this hashMap = { 2: 0, 7: 1, 11: 2, 15: 3 }
+  for (i = 0; i < nums.length; i++) {
+    let currentNum = nums[i];
+    let neededValue = target - currentNum;
+
+    // check to see key exists
+    if (hashMap[neededValue] === undefined) { // if neededValue key does not exist in hashmap
+      hashMap[currentNum] = i; // set neededValue in hashmap
+    } else { // if neededValue key exists in the hashMap
+      return [hashMap[neededValue], i]; // return value (index)
     }
+  }
+  // If no sum found for target throw and error
+  throw new Error("No nums in array add up to target");
 };
 
 module.exports = twoSum;
--- src/leetcode/1-two-sum/two-sum.test.js
+++ src/leetcode/1-two-sum/two-sum.test.js
@@ -1,10 +1,32 @@
-const { expect } = require("@jest/globals");
 const twoSum = require("./two-sum")
 
-
-it("Test case 1", () => {
+it("Test case 1: nums = [2, 7, 11, 15], target = 9 | should return [0, 1]", () => {
   const nums = [2, 7, 11, 15];
   const target = 9;
-//   expect("lala").toBe("lala")
   expect(twoSum(nums, target)).toEqual([0, 1]);
 });
+
+it("Test case 2: nums = [2, 7, 11, 15], target = 22 | should return [1, 3]", () => {
+  const nums = [2, 7, 11, 15];
+  const target = 22;
+  expect(twoSum(nums, target)).toEqual([1, 3]);
+});
+
+it("Test case 3: nums = [2, 7, 11, 15], target = 100 | should throw an error", () => {
+  const nums = [2, 7, 11, 15];
+  const target = 100;
+  expect(() => twoSum(nums, target)).toThrow(Error);
+});
+
+it("Test case 4: nums = [3, 3], target = 6 | should return [0, 1]", () => {
+  const nums = [3, 3];
+  const target = 6;
+  expect(twoSum(nums, target)).toEqual([0, 1]);
+});
+
+it("Test case 5: nums = [3, 2, 4], target = 6 | should return [1, 2]", () => {
+  const nums = [3, 2, 4];
+  const target = 6;
+  expect(twoSum(nums, target)).toEqual([1, 2]);
+});
+
